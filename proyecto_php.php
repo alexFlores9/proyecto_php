ad41399e114babb6da05239da0d382b4b5916ecc
alexFlores9 <aleexflors123@gmail.com>
2020-05-15 16:34:35 -0600

    initial commit

---
+++ proyecto_php.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 6</title>
+</head>
+<body>
+<?php  
+$num=1; 
+
+while ($num<=200) { 
+
+print $num; 
+
+if ($num<=199)  { 
+
+print "-"; } 
+
+$num++; 
+} 
+?>
+</body>
+</html>
